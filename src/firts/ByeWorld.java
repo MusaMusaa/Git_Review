c2510050966627d1b55d925c0d4355e596a55b80
Musa Geyik <geyik.musa0@gmail.com>
2022-08-23 08:31:22 -0700

    added bye world

---
+++ src/firts/ByeWorld.java
@@ -0,0 +1,8 @@
+package firts;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("5 weeks lefts");
+        System.out.println("1.5 week of git");
+    }
+}
